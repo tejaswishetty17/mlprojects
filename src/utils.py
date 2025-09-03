efe8e0b52b1e4fba5743a047cb599d9faac3b83c
tejaswishetty17 <shettytejaswi17@gmail.com>
2025-09-03 16:02:46 +0530

    predict pipeline

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import sys
 import dill
+import pickle
 from sklearn.metrics import r2_score
 from src.exception import CustomException
 from sklearn.model_selection import GridSearchCV
@@ -42,5 +43,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
         return report
 
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
